4600b15ba1afbe31a1cf1111bbc1cd5a7b0bfc11
DarkSwarp <113368991+DarkSwarp@users.noreply.github.com>
2023-06-14 11:36:52 +0300

    Add files via upload

---
+++ src/Redux/store.js
@@ -0,0 +1,37 @@
+import { configureStore } from '@reduxjs/toolkit';
+import { authReducer } from './auth/authSlice';
+import { contactsReducer } from './contacts/contactsSlice';
+import { filterReducer } from './contacts/filterSlice';
+import storage from 'redux-persist/lib/storage'; 
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+
+const persistConfig = {
+  key: 'auth',
+  storage,
+  whitelist: ['token'],
+};
+
+export const store = configureStore({
+  reducer: {
+    auth: persistReducer(persistConfig, authReducer),
+    filter: filterReducer,
+    contacts: contactsReducer,
+  },
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+export const persistor = persistStore(store);
